0579099242bc66eb5355e56f1fae88350db5588e
JavierCisterna <javiercisternameza@gmail.com>
2023-11-11 19:55:00 -0300

    Ruteo Correo

--- src/app/pages/ingreso/ingreso.page.ts
+++ src/app/pages/ingreso/ingreso.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { DataBaseService } from 'src/app/services/data-base.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ingreso',
@@ -17,7 +18,8 @@ export class IngresoPage implements OnInit {
   correo = 'atorres@duocuc.cl';
   password = '1234';
 
-  constructor(private bd: DataBaseService, private authService: AuthService) { }
+  constructor(private bd: DataBaseService, private authService: AuthService, private router: Router) { }
+  
 
   async ngOnInit() {
     this.bd.crearUsuariosDePrueba().then(async () => {
@@ -29,4 +31,8 @@ export class IngresoPage implements OnInit {
     this.authService.login(this.correo, this.password);
   }
 
+  recuperarContrasena() {
+    // Utiliza el servicio de enrutamiento para redirigir a otra página
+    this.router.navigate(['correo']);
+  }
 }
